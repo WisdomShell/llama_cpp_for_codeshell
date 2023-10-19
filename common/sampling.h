7382f2618ea46bba4075de400d10a0f89d6b9530
chiranko <2057773822@qq.com>
2023-10-19 04:52:15 +0000

    fix default temp

--- common/sampling.h
+++ common/sampling.h
@@ -12,7 +12,7 @@ typedef struct llama_sampling_params {
     float   top_p             = 0.95f; // 1.0 = disabled
     float   tfs_z             = 1.00f; // 1.0 = disabled
     float   typical_p         = 1.00f; // 1.0 = disabled
-    float   temp              = 0.80f; // 1.0 = disabled
+    float   temp              = 0.20f; // 1.0 = disabled
     float   repeat_penalty    = 1.00f; // 1.0 = disabled
     int32_t repeat_last_n     = 64;    // last n tokens to penalize (0 = disable penalty, -1 = context size)
     float   frequency_penalty = 0.00f; // 0.0 = disabled
